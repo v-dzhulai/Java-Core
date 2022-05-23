e1ea4211e3301b8523526a0aa9a06a3f9924b9f2
Mr.V <dzulajvita@gmail.com>
2022-05-23 21:37:12 +0300

    DailyPlanner

---
+++ DailyPlanner/src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        String arr[] = {"wake up", "have breakfast", "go to work", "solve 5 tasks from codewars"};
+
+        System.out.println(createDailyPlan(new String[] {}));
+    }
+
+    public static String createDailyPlan(String[] plans) {
+        StringBuilder str = new StringBuilder().append("My plans: \n");
+
+        for(int i = 0; i < plans.length; i++) {
+            str.append(i + 1 + ".) " + plans[i]);
+            str.append(i == plans.length - 1 ? ";" : ";\n");
+        }
+
+        return str.toString();
+    }
+}
